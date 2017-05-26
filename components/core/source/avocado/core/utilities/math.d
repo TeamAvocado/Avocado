752dabe65c391bc143c729125c79b235a8e80e83
WebFreak001 <janju007@web.de>
2017-05-26 16:51:45 +0200

    Format core.utilities.math

--- components/core/source/avocado/core/utilities/math.d
+++ components/core/source/avocado/core/utilities/math.d
@@ -1,21 +1,18 @@
 module avocado.core.utilities.math;
 
-import gl3n.linalg;
-import gl3n.math;
+public import gl3n.linalg;
+public import gl3n.math;
 
 quat lookAt(vec3 source, vec3 dest, vec3 up)
-in
-{
+in {
 	assert(abs(source.length_squared - 1.0f) < 0.00001f, "Source must be normalized");
 	assert(abs(dest.length_squared - 1.0f) < 0.00001f, "Dest must be normalized");
 	assert(abs(up.length_squared - 1.0f) < 0.00001f, "Up must be normalized");
 }
-out (res)
-{
-	assert(res.isFinite, "Invalid quaternion " ~ (cast() res).toString);
+out (res) {
+	assert(res.isFinite, "Invalid quaternion " ~ (cast()res).toString);
 }
-body
-{
+body {
 	float d = dot(source, dest);
 	if (abs(d + 1.0f) < 0.0001f) // d == -1
 		return quat(cradians!180, up);
@@ -26,19 +23,16 @@ body
 
 /// Helper calling atan2(source.y - dest.y, source.x - dest.x)
 /// 0 = facing towards the X axis
-float lookAt(vec2 source, vec2 dest)
-{
+float lookAt(vec2 source, vec2 dest) {
 	return atan2(source.y - dest.y, source.x - dest.x);
 }
 
-float cos_lerp(T)(T a, T b, float t)
-{
+float cos_lerp(T)(T a, T b, float t) {
 	t = (1 - cos(t * 3.1415926f)) * 0.5f;
 	return a * (1 - t) + b * t;
 }
 
-unittest
-{
+unittest {
 	assert(abs(cos_lerp(0, 1, 0) - 0) < 0.00001f);
 	assert(abs(cos_lerp(0, 1, 1) - 1) < 0.00001f);
 	assert(abs(cos_lerp(0, 1, 0.5f) - 0.5f) < 0.00001f);
