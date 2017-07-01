9f92adadbe63af01d2c03b0c8cc6394744ab1e7a
WebFreak001 <janju007@web.de>
2017-07-01 23:02:44 +0200

    made GLShaderUnit a ResourceProvider

--- components/gl3/source/avocado/gl3/gl3shader.d
+++ components/gl3/source/avocado/gl3/gl3shader.d
@@ -1,5 +1,6 @@
 module avocado.gl3.gl3shader;
 
+import avocado.core.resource.resourceprovider;
 import avocado.core.display.irenderer;
 import avocado.core.display.ishader;
 import avocado.core.util;
@@ -29,11 +30,22 @@ enum uniformRegex = ctRegex!`(?<!// *)uniform\s+[a-zA-Z_][0-9a-zA-Z_]*\s+([a-zA-
 
 /// Compiled shader part (vertex, fragment, etc.)
 /// Checks for duplicate shaders in debug mode
-class GLShaderUnit {
+class GLShaderUnit : IResourceProvider {
 public:
 	/// Creates & compiles a shader
 	/// Checks for duplicates using a md5 checksum in debug mode
 	this(ShaderType type, string content, bool scanUniforms = true) {
+		_type = type;
+		_scanUniforms = scanUniforms;
+		load(content);
+	}
+
+	this(ShaderType type, bool scanUniforms = true) {
+		_type = type;
+		_scanUniforms = scanUniforms;
+	}
+
+	void load(string content) {
 		debug {
 			import std.algorithm;
 			import std.digest.md;
@@ -43,7 +55,7 @@ public:
 			hashSums ~= hash;
 			_content = content;
 		}
-		_id = glCreateShader(type);
+		_id = glCreateShader(_type);
 		if (_id == 0) {
 			_success = false;
 			_msg = "Error in glCreateShader";
@@ -61,17 +73,17 @@ public:
 			char* log = new char[logSize].ptr;
 			glGetShaderInfoLog(_id, logSize, &logSize, &log[0]);
 
-			_msg = "Error in glCompileShader for " ~ type.to!string ~ "Shader:\n" ~ log[0 .. logSize].idup;
+			_msg = "Error in glCompileShader for " ~ _type.to!string ~ "Shader:\n" ~ log[0 .. logSize].idup;
 			_success = false;
 			return;
 		}
-		if (scanUniforms)
+		if (_scanUniforms)
 			foreach (match; matchAll(content, uniformRegex))
 				_uniforms ~= match[1];
 	}
 
 	/// Returns the error message (if any)
-	string error() @property {
+	string errorMessage() @property {
 		return _msg;
 	}
 
@@ -95,12 +107,33 @@ public:
 		return _content;
 	}
 
+	/// Unused
+	void error() {
+	}
+	/// Unused
+	@property string errorInfo() {
+		return "";
+	}
+
+	/// Loads a resource from a memory stream
+	bool load(ref ubyte[] stream) {
+		load(cast(string)stream.idup);
+		return true;
+	}
+
+	/// Always true
+	bool canRead(string extension) {
+		return true;
+	}
+
 private:
 	debug static ubyte[16][] hashSums;
 	uint _id;
 	bool _success = true;
 	string[] _uniforms;
 	string _msg, _content;
+	ShaderType _type;
+	bool _scanUniforms;
 }
 
 class GL3ShaderProgram : IShader {
@@ -115,11 +148,11 @@ public:
 			attach(unit);
 		create(renderer);
 		foreach (unit; units)
-			register(unit.uniforms);
+			register(unit.uniforms, false);
 	}
 
 	GL3ShaderProgram attach(GLShaderUnit shader) {
-		assert(shader.success, shader.error);
+		assert(shader.success, shader.errorMessage);
 		if (shader.success) {
 			glAttachShader(_program, shader.id);
 		}
@@ -145,10 +178,13 @@ public:
 		glUseProgram(_program);
 	}
 
-	void set(T)(string uniform, T value) {
+	void set(T)(string uniform, T value, bool throwError = true) {
 		import std.traits;
 
-		assert(uniform in _uniforms, "Uniform '" ~ uniform ~ "' does not exist. Did you register it?");
+		if (throwError)
+			assert(uniform in _uniforms, "Uniform '" ~ uniform ~ "' does not exist. Did you register it?");
+		else if (!(uniform in _uniforms))
+			return;
 		enforceGLErrors("An error occured previously!\n");
 
 		static if (isArray!T) {
@@ -159,17 +195,17 @@ public:
 			else static if (is(U == float))
 				glUniform1fv(_uniforms[uniform], cast(int)value.length, value.ptr);
 			else static if (is(U == vec2))
-				glUniform2fv(_uniforms[uniform], cast(int)value.length, cast(float*) value.ptr);
+				glUniform2fv(_uniforms[uniform], cast(int)value.length, cast(float*)value.ptr);
 			else static if (is(U == vec3))
-				glUniform3fv(_uniforms[uniform], cast(int)value.length, cast(float*) value.ptr);
+				glUniform3fv(_uniforms[uniform], cast(int)value.length, cast(float*)value.ptr);
 			else static if (is(U == vec4))
-				glUniform4fv(_uniforms[uniform], cast(int)value.length, cast(float*) value.ptr);
+				glUniform4fv(_uniforms[uniform], cast(int)value.length, cast(float*)value.ptr);
 			else static if (is(U == mat2))
-				glUniformMatrix2fv(_uniforms[uniform], cast(int)value.length, 1, cast(float*) value.ptr);
+				glUniformMatrix2fv(_uniforms[uniform], cast(int)value.length, 1, cast(float*)value.ptr);
 			else static if (is(U == mat3))
-				glUniformMatrix3fv(_uniforms[uniform], cast(int)value.length, 1, cast(float*) value.ptr);
+				glUniformMatrix3fv(_uniforms[uniform], cast(int)value.length, 1, cast(float*)value.ptr);
 			else static if (is(U == mat4))
-				glUniformMatrix4fv(_uniforms[uniform], cast(int)value.length, 1, cast(float*) value.ptr);
+				glUniformMatrix4fv(_uniforms[uniform], cast(int)value.length, 1, cast(float*)value.ptr);
 			else
 				static assert(0, "Invalid shader argument type: " ~ T.stringof);
 		} else {
@@ -192,20 +228,22 @@ public:
 			else
 				static assert(0, "Invalid shader argument type: " ~ T.stringof);
 		}
-		enforceGLErrors("Error assigning uniform value. Please ensure this shader is currently bound. (for " ~ uniform ~ ")\n");
+		if (throwError)
+			enforceGLErrors("Error assigning uniform value. Please ensure this shader is currently bound. (for " ~ uniform ~ ")\n");
 	}
 
-	void registerUniform(string uniform) {
+	void registerUniform(string uniform, bool throwError = true) {
 		if (uniform in _uniforms)
 			return;
 		immutable location = glGetUniformLocation(_program, uniform.toStringz);
-		assert(location != -1, "Uniform '" ~ uniform ~ "' does not exist in shader program or is reserved!");
+		if (throwError)
+			assert(location != -1, "Uniform '" ~ uniform ~ "' does not exist in shader program or is reserved!");
 		_uniforms[uniform] = location;
 	}
 
-	void register(string[] uniforms) {
+	void register(string[] uniforms, bool throwError = true) {
 		foreach (uniform; uniforms)
-			registerUniform(uniform);
+			registerUniform(uniform, throwError);
 	}
 
 	auto id() @property {
